9bb1f5070a075fec653d64284e83977af61c156e
Smollny <smolllny@gmail.com>
2022-02-11 23:25:39 +0300

    Done

---
+++ Task15.6.6/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Task15._6._6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var classes = new[]
+            {
+               new Classroom { Students = {"Evgeniy", "Sergey", "Andrew"}, },
+               new Classroom { Students = {"Anna", "Viktor", "Vladimir"}, },
+               new Classroom { Students = {"Bulat", "Alex", "Galina"}, }
+           };
+
+            var allStudents = GetAllStudents(classes);
+
+            Console.WriteLine(string.Join(" ", allStudents));
+            Console.ReadKey();
+        }
+
+        static string[] GetAllStudents(Classroom[] classes)
+        {
+            var result = new List<string>();
+            foreach (var item in classes)
+            {
+                result.AddRange(item.Students);
+            }
+            return result.Distinct().ToArray();
+        }
+    }
+    public class Classroom
+    {
+        public List<string> Students = new List<string>();
+    }
+}
